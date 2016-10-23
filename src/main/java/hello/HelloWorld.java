6cf6ee5d9f33ef7cf0be07cdeb0af3e9142ff852
Aaron <culnoty@outlook.com>
2016-10-23 13:06:21 +0800

    add dependency

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,8 +1,12 @@
 package hello;
 
+import org.joda.time.LocalTime;
 
 public class HelloWorld{
     public static void main(String[] args){
+        LocalTime currentTime = new LocalTime();
+        System.out.println("the current time:" +currentTime);
+
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
