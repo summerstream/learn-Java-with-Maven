46ea3e82f2e627a07aba65a966afe138fa357c63
Aaron <culnoty@outlook.com>
2016-10-23 18:00:10 +0800

    spring container

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
+public class Application{
+    @Bean
+    MessageService mockMessageService(){
+        return new MessageService(){
+            public String getMessage(){
+                return "Hello.world.";
+            }
+        };
+    }
+
+    public static void main(String[] args){
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
+    }
+}
